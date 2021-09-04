5ca672ec001424b19203c8c78f3099e7174dc37f
Brian Vehige <brianvehige@gmail.com>
2021-09-03 18:42:11 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (
+            <div>
+              <h3>{'Title: ' + movie.title}</h3>
+              <p>{`Showtime: ${movie.time} minutes`}</p>
+              <p>Genres:</p>
+              <ul>
+                {movie.genres.map(genre => <li>{genre}</li>)}
+              </ul>
+              </div>
+          )
+        })}
     </div>
   );
 };
